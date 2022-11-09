ee70c289e1ab576b0eda51caf898b7572b66a30b
SeraphJACK <seraphjack@outlook.com>
2022-11-09 18:51:14 +0800

    Auth message

--- game/message.go
+++ game/message.go
@@ -4,3 +4,8 @@ type Message struct {
 	Event string                 `json:"event"`
 	Data  map[string]interface{} `json:"data"`
 }
+
+type AuthData struct {
+	Slots uint   `json:"slots" mapstructure:"slots"`
+	Token string `json:"token" mapstructure:"token"`
+}
