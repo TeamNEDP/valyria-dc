b9e09cf513037f1b87139cd297f229e25462c5b6
SeraphJACK <seraphjack@outlook.com>
2022-11-13 23:13:37 +0800

    fix

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"log"
+	"math/rand"
+	"time"
 	"valyria-dc/game"
 	"valyria-dc/services"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	game.InitWatchdog()
 
 	err := services.Start("0.0.0.0:8000")
